70b658d440326be6433c38cc8d5461d3b8b3c45a
Permetris <jboguno@gmail.com>
2022-04-10 14:08:10 +0200

    transfer

--- src/CalculateKey.js
+++ src/CalculateKey.js
@@ -1,74 +1,79 @@
-// const isKey = (key, attributes, dependencies) => {
-//   let bracket = key;
-//   let dependenciesN = dependencies.map((el) => el);
+const isKey = (key, attributes, dependencies) => {
+  let bracket = key;
+  let dependenciesN = dependencies.map((el) => el);
 
-//   while (true) {
+    let arr1 = [1, 2, 3];
+    let arr2 = [1, 2, 3];
+
+    console.log(arr1.toString());
+//   while () {
 //     for (let i = 0; i < dependenciesN.length; i++) {
-//       if (key.includes(...dependenciesN[i].left)) {
+//         if (key.includes(...dependenciesN[i].left)) {
+          
 //         bracket.push(...dependenciesN[i].right);
 
 //         dependenciesN.splice(i, 1);
-//           console.log(bracket);
+//         console.log(bracket);
 //         break;
 //       }
 //     }
 //   }
 
-//   return true;
-// };
+  return true;
+};
 
 const CalculateKey = (schema) => {
-    const attributes = schema.attributes.map((el) => {
-        return { value: el, left: false, right: false };
-    });
-    const dependencies = schema.dependencies;
+  const attributes = schema.attributes.map((el) => {
+    return { value: el, left: false, right: false };
+  });
+  const dependencies = schema.dependencies;
 
-    let onRightSide = [];
-    let onLeftSide = [];
-    let onBothSides = [];
-    let notInDependencies = [];
-    let key = [];
+  let onRightSide = [];
+  let onLeftSide = [];
+  let onBothSides = [];
+  let notInDependencies = [];
+  let key = [];
 
-    for (let attribute of attributes) {
-        for (let dependency of dependencies) {
-            for (let leftValue of dependency.left) {
-                if (attribute.value === leftValue) {
-                    attribute.left = true;
-                }
-            }
-            for (let rightValue of dependency.right) {
-                if (attribute.value === rightValue) {
-                    attribute.right = true;
-                }
-            }
+  for (let attribute of attributes) {
+    for (let dependency of dependencies) {
+      for (let leftValue of dependency.left) {
+        if (attribute.value === leftValue) {
+          attribute.left = true;
+          break;
         }
-    }
-
-    for (let attribute of attributes) {
-        if (attribute.left === false && attribute.right === false) {
-            notInDependencies.push(attribute.value);
-        } else if (attribute.left === false && attribute.right === true) {
-            onRightSide.push(attribute.value);
-        } else if (attribute.left === true && attribute.right === false) {
-            onLeftSide.push(attribute.value);
-        } else {
-            onBothSides.push(attributes.value);
+      }
+      for (let rightValue of dependency.right) {
+        if (attribute.value === rightValue) {
+          attribute.right = true;
+          break;
         }
+      }
     }
+  }
 
-    key.push(...onLeftSide, ...notInDependencies);
+  for (let attribute of attributes) {
+    if (attribute.left === false && attribute.right === false) {
+      notInDependencies.push(attribute.value);
+    } else if (attribute.left === true && attribute.right === false) {
+      onLeftSide.push(attribute.value);
+    } else {
+      onBothSides.push(attributes.value);
+    }
+  }
+
+  key.push(...onLeftSide, ...notInDependencies);
+  //   return key;
+  if (isKey(key, schema.attributes, dependencies)) {
     return key;
-    //   if (isKey(key, schema.attributes, dependencies)) {
-    //     return key;
-    //   }
+  }
 
-    //   for (let attribute of onBothSides) {
-    //     key.push(attribute);
-    //     if (isKey(key, schema.attributes, dependencies)) {
-    //       return key;
-    //     }
-    //   }
-    // };
+  //   for (let attribute of onBothSides) {
+  //     key.push(attribute);
+  //     if (isKey(key, schema.attributes, dependencies)) {
+  //       return key;
+  //     }
+  //   }
+  // };
 };
 
 export default CalculateKey;
